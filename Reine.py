2044a902fa77eda1e5be1ec0fbd2efcf96d9c98b
Senniinn <39193668+Senniinn@users.noreply.github.com>
2019-10-17 10:45:09 +0200

    oui

---
+++ Reine.py
@@ -0,0 +1,14 @@
+from Pion import Pion
+
+class Reine(Pion):
+    def __init__(self, x, y, color, canvas):
+        self.color = color
+        self.x = x
+        self.y = y
+        self.canvas = canvas
+        self.oval = self.canvas.create_oval(self.x + 5, self.y + 5, self.x + 55, self.y + 55, fill=self.color)
+
+    def move(self, x, y):
+        self.x = x
+        self.y = y
+        self.can.coords(self.oval, self.x + 5, self.y + 5, self.x + 55, self.y + 55)
