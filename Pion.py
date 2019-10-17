bee022daabb95f0104772865d1a7fc433a08485d
Senniinn <39193668+Senniinn@users.noreply.github.com>
2019-10-17 12:24:15 +0200

    test des possibilité

--- Pion.py
+++ Pion.py
@@ -11,12 +11,10 @@ class Pion():
         possiblity2 = [pion.x-60, pion.y-60]
         xmin = pion.x-60
         xmax = pion.x+60
-        ymin = pion.y-60
-        ymax = pion.y+60
         possiblities = []
 
         for pionsBlanc in pionsBlancs:
-            if (pionsBlanc.x >= xmin and pionsBlanc.x <= xmax and pionsBlanc.y >= ymin and pionsBlanc.y <= ymax):
+            if (pionsBlanc.x >= xmin and pionsBlanc.x <= xmax):
                 if (possiblity1[0] >= 0):
                     if (possiblity1[0] != pionsBlanc.x and  possiblity1[1] != pionsBlanc.y):
                         if possiblity1 not in possiblities:
@@ -26,7 +24,7 @@ class Pion():
                         if possiblity2 not in possiblities:
                             possiblities.append(possiblity2)
         for pionsNoir in pionsNoirs:
-            if (pionsNoir.x >= xmin and pionsNoir.x <= xmax and pionsNoir.y >= ymin and pionsNoir.y <= ymax):
+            if (pionsNoir.x >= xmin and pionsNoir.x <= xmax):
                 if (possiblity1[0] >= 0):
                     if (possiblity1[0] != pionsNoir.x and  possiblity1[1] != pionsNoir.y):
                         if possiblity1 not in possiblities:
