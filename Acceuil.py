c278389414c663f6391d83bbe445117ccd8bab03
tibolino <thibault.linossier@epsi.fr>
2019-10-18 15:39:27 +0200

    commit

--- Acceuil.py
+++ Acceuil.py
@@ -1,27 +1,29 @@
 from tkinter import *
-
+from tkinter.scrolledtext import *
+import webbrowser
 from Dame import Dame
 
 
 def lauchGame():
     fen1=Tk()
+    root.destroy()
     can1 = Canvas(fen1, bg='dark grey', height=600, width=600)
     can1.pack(side=LEFT)
     d = Dame(can1)
     d.creerDamier()
-    root.quit()
     fen1.mainloop()
     fen1.destroy()
+def rules():
+    webbrowser.open("http://www.ffjd.fr/Web/index.php?page=reglesdujeu")
 
 
 root = Tk()
-creer = Button(root, text='Jouer', anchor="center",bg='green',padx=20,pady=20 ,command=lauchGame())
-creer.pack()
-bou1 = Button(root, text='Quitter',anchor="center",bg='red',padx=20,pady=20, command=root.quit)
-bou1.pack()
-root.geometry("800x600+300+0")
-
-
-
+boutcre = Button(root, text='Jouer',command=lauchGame, anchor="center",bg='green',padx=20,pady=20 )
+boutcre.pack(side=TOP, anchor=W, fill=X, expand=YES)
+boutrules = Button(root, text='Régles',command=rules, anchor="center",bg='blue',padx=20,pady=20 )
+boutrules.pack(side=TOP, anchor=W, fill=X, expand=YES)
+bouquit = Button(root, text='Quitter',anchor="center",bg='red',padx=20,pady=20, command=root.quit)
+bouquit.pack(side=TOP, anchor=W, fill=X, expand=YES)
+root.geometry("500x200+150+0")
 root.mainloop()  # démarrage du réceptionnaire d'événement
 root.destroy()
