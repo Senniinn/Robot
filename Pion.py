8fc7df5b4eb7cdb6f01b0fd38f63fc0b6c7a0fbf
Senniinn <39193668+Senniinn@users.noreply.github.com>
2019-10-19 13:18:31 +0200

    Add features back dash eat, can move but can't eat

--- Pion.py
+++ Pion.py
@@ -1,5 +1,5 @@
 class Pion():
-    def __init__(self,x, y, color, canvas):
+    def __init__(self, x, y, color, canvas):
         self.color = color
         self.x = x
         self.y = y
@@ -8,30 +8,47 @@ class Pion():
 
     def possibilities(self, pionsBlancs, pionsNoirs, pion):
 
-##########  DECLARATION DES VARRIBLES  ##########
+        ##########  DECLARATION DES VARRIBLES  ##########   60 = une unité
 
         if pion.color == "white":
-            possiblity1 = [pion.x + 60, pion.y - 60] #Avancer de vers la droite
-            possiblity1a = [pion.x + 120, pion.y - 120] #Manger vers la droire
-            possiblity2 = [pion.x - 60, pion.y - 60] #Avancer de vers la gauche
-            possiblity2a = [pion.x - 120, pion.y - 120] #Manger vers la gauche
+            possiblity1 = [pion.x + 60, pion.y - 60]  # Avancer de vers la droite
+            possiblity2 = [pion.x - 60, pion.y - 60]  # Avancer de vers la gauche
+            possiblity1a = [pion.x + 120, pion.y - 120]  # Manger vers la droite
+            possiblity2a = [pion.x - 120, pion.y - 120]  # Manger vers la gauche
+
+            possiblity3 = [pion.x + 60, pion.y + 60]  # Check derriere a droite
+            possiblity4 = [pion.x - 60, pion.y + 60]  # Check derriere a gauche
+            possiblity3a = [pion.x + 120, pion.y + 120]  # Manger derriere a droite
+            possiblity4a = [pion.x - 120, pion.y + 120]  # Manger derriere a gauche
         else:
-            possiblity1 = [pion.x - 60, pion.y + 60] #Avancer de vers la gauche
-            possiblity1a = [pion.x - 120, pion.y + 120] #Manger vers la gauche
-            possiblity2 = [pion.x + 60, pion.y + 60] #Avancer de vers la droite
-            possiblity2a = [pion.x + 120, pion.y + 120] #Manger vers la droite
+            possiblity1 = [pion.x - 60, pion.y + 60]  # Avancer de vers la gauche
+            possiblity2 = [pion.x + 60, pion.y + 60]  # Avancer de vers la droite
+            possiblity1a = [pion.x - 120, pion.y + 120]  # Manger vers la gauche
+            possiblity2a = [pion.x + 120, pion.y + 120]  # Manger vers la droite
+
+            possiblity3 = [pion.x + 60, pion.y - 60]  # Check derriere a droite
+            possiblity4 = [pion.x - 60, pion.y - 60]  # Check derriere a gauche
+            possiblity3a = [pion.x + 120, pion.y - 120]  # Manger derriere a droite
+            possiblity4a = [pion.x - 120, pion.y - 120]  # Manger derriere a gauche
 
         validPoss1 = True
         validPoss2 = True
         validPoss1a = False
         validPoss2a = False
-        possiblities = [] # Tableau qui va contenir les différentes possibilité d'un pion
+
+        validPoss3 = True
+        validPoss4 = True
+        validPoss3a = False
+        validPoss4a = False
+        possiblities = []  # Tableau qui va contenir les différentes possibilité d'un pion
         pionDown = None
 
 
-##########  CHECK DES DEPLACEMENTS ##########
 
-       # Checks des pions blanc       qu'on soit un pion noir ou blanc
+
+        ##########  CHECK DES DEPLACEMENTS ##########
+
+        # Checks des pions blanc       qu'on soit un pion noir ou blanc
         for pionsBlanc in pionsBlancs:
             if possiblity1[0] >= 0 and possiblity1[0] == pionsBlanc.x and possiblity1[1] == pionsBlanc.y:  # Check si un pion blanc est a droite
                 validPoss1 = False
@@ -39,51 +56,85 @@ class Pion():
                     validPoss1a = True  # Possibilité de manger vers la droite passe a vrai  ==> Voir plus bas
                     pionDown = pionsBlanc  # Le pion manger sera donc un pion blanc
 
-            if possiblity2[0] <= 540 and possiblity2[0] == pionsBlanc.x and possiblity2[
-                1] == pionsBlanc.y:  # Check si un pion blanc est a gauche
+            if possiblity2[0] <= 540 and possiblity2[0] == pionsBlanc.x and possiblity2[1] == pionsBlanc.y:  # Check si un pion blanc est a gauche
                 validPoss2 = False
                 if pion.color != pionsBlanc.color:
                     validPoss2a = True
                     pionDown = pionsBlanc
 
+            if possiblity3[0] >= 0 and possiblity3[0] == pionsBlanc.x and possiblity3[1] == pionsBlanc.y:  # Check si un pion blanc est derriere a droite
+                if pion.color != pionsBlanc.color:
+                    validPoss3a = True
+                    pionDown = pionsBlanc
+
+            if possiblity4[0] <= 540 and possiblity4[0] == pionsBlanc.x and possiblity4[1] == pionsBlanc.y:  # Check si un pion blanc est derriere a gauche
+                if pion.color != pionsBlanc.color:
+                    validPoss4a = True
+                    pionDown = pionsBlanc
 
         # Checks des pions Noirs      qu'on soit un pion noir ou blanc              Même raisonnement que plus haut
         for pionsNoir in pionsNoirs:
             if possiblity1[0] >= 0 and possiblity1[0] == pionsNoir.x and possiblity1[1] == pionsNoir.y:
                 validPoss1 = False
-                if pion.color != pionsNoir.color: # Si on est un blanc
+                if pion.color != pionsNoir.color:  # Si on est un blanc
                     validPoss1a = True
                     pionDown = pionsNoir
 
             if possiblity2[0] <= 540 and possiblity2[0] == pionsNoir.x and possiblity2[1] == pionsNoir.y:
                 validPoss2 = False
-                if pion.color != pionsNoir.color: # Si on est un blanc
+                if pion.color != pionsNoir.color:  # Si on est un blanc
                     validPoss2a = True
                     pionDown = pionsNoir
 
-##########  CHECK SI ON PEUT MANGER UN PION ##########
+            if possiblity3[0] >= 0 and possiblity3[0] == pionsNoir.x and possiblity3[1] == pionsNoir.y:
+                if pion.color != pionsNoir.color:
+                    validPoss3a = True
+                    pionDown = pionsNoir
 
-        if validPoss1a:  #La possibilité est vraie, elle deviendra fausse a la fin si, on trouve un pion sur cette case
+            if possiblity4[0] <= 540 and possiblity4[0] == pionsNoir.x and possiblity4[1] == pionsNoir.y:
+                if pion.color != pionsNoir.color:
+                    validPoss4a = True
+                    pionDown = pionsNoir
+
+
+
+        ##########  CHECK SI ON PEUT MANGER UN PION ##########
+
+        if validPoss1a:  # La possibilité est vraie, elle deviendra fausse a la fin si, on trouve un pion sur cette case
             for pionsBlanc in pionsBlancs:
                 if possiblity1a[0] >= 0 and possiblity1a[0] == pionsBlanc.x and possiblity1a[1] == pionsBlanc.y:
                     validPoss1a = False
-                    pionDown = None
-            for pionsNoir in pionsNoirs: #Même raisonnement pour un pion noir
+            for pionsNoir in pionsNoirs:  # Même raisonnement pour un pion noir
                 if possiblity1a[0] >= 0 and possiblity1a[0] == pionsNoir.x and possiblity1a[1] == pionsNoir.y:
                     validPoss1a = False
-                    pionDown = None
-        if validPoss2a: #Même raisonnement mais pour la possibilité deux, à savoir le déplacement vers la gauche
+        if validPoss2a:  # Même raisonnement mais pour la possibilité deux, à savoir le déplacement vers la gauche
             for pionsBlanc in pionsBlancs:
                 if possiblity2a[0] <= 540 and possiblity2a[0] == pionsBlanc.x and possiblity2a[1] == pionsBlanc.y:
                     validPoss2a = False
-                    pionDown = None
             for pionsNoir in pionsNoirs:
-                if possiblity2[0] <= 540 and possiblity2a[0] == pionsNoir.x and possiblity2a[1] == pionsNoir.y:
+                if possiblity2a[0] <= 540 and possiblity2a[0] == pionsNoir.x and possiblity2a[1] == pionsNoir.y:
                     validPoss2a = False
-                    pionDown = None
+        if validPoss3a:
+            for pionsBlanc in pionsBlancs:
+                if possiblity3a[0] <= 0 and possiblity3a[0] == pionsBlanc.x and possiblity3a[1] == pionsBlanc.y:
+                    validPoss3a = False
+            for pionsNoir in pionsNoirs:
+                if possiblity3a[0] <= 0 and possiblity3a[0] == pionsNoir.x and possiblity3a[1] == pionsNoir.y:
+                    validPoss3a = False
+        if validPoss4a:
+            for pionsBlanc in pionsBlancs:
+                if possiblity4a[0] <= 540 and possiblity4a[0] == pionsBlanc.x and possiblity4a[1] == pionsBlanc.y:
+                    validPoss4a = False
+            for pionsNoir in pionsNoirs:
+                if possiblity4a[0] <= 540 and possiblity4a[0] == pionsNoir.x and possiblity4a[1] == pionsNoir.y:
+                    validPoss4a = False
 
 
-# Passage des possibilités au tableau.
+
+
+
+
+        # Passage des possibilités au tableau.
         if validPoss1:
             possiblities.append(possiblity1)
         if validPoss2:
@@ -92,12 +143,17 @@ class Pion():
             possiblities.append(possiblity1a)
         if validPoss2a:
             possiblities.append(possiblity2a)
+        if validPoss3a:
+            possiblities.append(possiblity3a)
+        if validPoss4a:
+            possiblities.append(possiblity4a)
 
-        return [possiblities, pionDown] #Retour des différentes possibilité et du pion manger.
+
+
+        return [possiblities, pionDown]  # Retour des différentes possibilité et du pion manger.
 
     def move(self, x, y):
         self.x = x
         self.y = y
         self.canvas.tag_raise(self.oval)
         self.canvas.coords(self.oval, self.x + 5, self.y + 5, self.x + 55, self.y + 55)
-
